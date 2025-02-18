6c5d83f37a60e7d595b1e8b6f408434fabee0959
Ivanov_Nikita <poldnik9999@yandex.ru>
2025-02-18 12:01:06 +0500

    дело сделано x2

--- src/test/java/pagesTest/CucumberRunnerTest.java
+++ src/test/java/pagesTest/CucumberRunnerTest.java
@@ -10,7 +10,7 @@ import static io.cucumber.core.options.Constants.*;
 @SelectClasspathResource("src/main/java/steps")
 @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@rar")
+//@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "@rar")
 @ConfigurationParameter(key = EXECUTION_DRY_RUN_PROPERTY_NAME, value = "FALSE")
 
 public class CucumberRunnerTest {
